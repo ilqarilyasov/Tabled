837be7186c48f61a8b694889189451183f071fc9
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-10 09:07:25 -0600

    Set up storyboard

---
+++ Tabled/Tabled/Resources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Tabled
+//
+//  Created by Ilgar Ilyasov on 1/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
